ada8796c9926b5b9e432777f1e8252cf56f945b9
Thilak <53122712+ThasthuThilak@users.noreply.github.com>
2020-07-18 11:51:10 +0530

    Update swayam.py

--- swayam.py
+++ swayam.py
@@ -1,8 +1,6 @@
 num = int(input())
-a = input()
 inpt1 = list(input())
 inpt2 = list(input())
-b = input()
 r = abs(inpt1.count('r')-inpt2.count('r'))
 m = abs(inpt1.count('m')-inpt2.count('m'))
 print(r+m)
